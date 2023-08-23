253119ac04f044e80e7d7a69b9693315853d70d3
Rustam25122512 <130824221+jkghdfjghdfhfdhg@users.noreply.github.com>
2023-08-23 20:59:33 +0600

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { carsReducer, addCar, removeCar, changeSearch} from "./slices/carsSlice";
+import { formReducer, changeName, changeCost } from "./slices/formSlice";
+import { searchReducer } from "./slices/searchSlice";
+
+
+const store = configureStore({
+    reducer: {
+        cars: carsReducer,
+        form: formReducer,
+        searchTerm: searchReducer,
+    },
+})
+
+export {store, addCar, removeCar, changeSearch, changeName, changeCost};
